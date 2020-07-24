fad06fda0c0ef235ade60c09791bf10ad68bed23
Karolina Kustron <karolina@cedita.com>
2020-07-24 19:31:48 +0100

    4.7

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -5,7 +5,7 @@
     <hr>
     <h2>Filtered Books by Ownership</h2>
     <select v-model="holding">
-
+    <option v-for="filter in filters"> {{filter}} </option>
     </select>
       <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
     </ul>
