db144374c6a1dcf9fa242f9064af6925fa1341c8
Karolina Kustron <karolina@cedita.com>
2020-07-24 17:41:28 +0100

    input

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -3,6 +3,9 @@
     <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
     <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
     <button type="submit" name="button">Add Book</button>
+    <div>
+      <input v-model=.../bookData.finishedReading>
+    </div>
   </form>
 </template>
 
